3b4ccbaaa195c7ea0e852d2fe25e763d7327d8ee
henry-lewek <lewek@hartford.edu>
2023-09-20 16:47:12 -0400

    removed 2nd scanner/formatted

--- Assignment1.java
+++ Assignment1.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-import Math.Random;
+
 
 public class Assignment1 {
   //-------------------------------------------
@@ -25,11 +25,11 @@ public class Assignment1 {
 
     Scanner input = new Scanner(System.in);
 
-    System.out.println("Please input a degree in Fahrenheit to convert to Celsius: "); // This line is asking for the user to input a number.
-    double fahrenheit = input.nextDouble();
+      System.out.println("Please input a degree in Fahrenheit to convert to Celsius: "); // This line is asking for the user to input a number.
 
+      double fahrenheit = input.nextDouble();
 
-    double  celsius =(( 5 *(fahrenheit - 32.0)) / 9.0); // Equation for converting F to C.
+      double celsius =(( 5 *(fahrenheit - 32.0)) / 9.0); // Equation for converting F to C.
 
     System.out.println("                       ");
 
@@ -37,13 +37,17 @@ public class Assignment1 {
     //  Code that allows the user to input a 5-character string, then reverses it.
     // ---------------------------------------------------------------------------
 
-    Scanner string = new Scanner(System.in);
-
     System.out.println("Next, please input a 5-character string:"); //Prompts user for 5 characters.
-    String character = string.nextLine();
 
-    String result = character.substring(1,4); // Removes first and last character in the string.
-    String reverse = new StringBuilder(result).reverse().toString(); // Reverses string.
+      String character = input.next();
+
+      String result = character.substring(1,4); // Removes first and last character in the string.
+
+      String reverse = new StringBuilder(result).reverse().toString(); // Reverses string.
+
+      System.out.println("                       ");
+
+      input.close();
 
     // ------------------------------------------------------
     //  Code that generates a random number from 32 - 16,384.
@@ -51,20 +55,32 @@ public class Assignment1 {
 
     System.out.println("Generating random number...");
 
-    int min = 32; // Minimum value
-    int max = 16384; // Maximum value
-    int random_int = (int)Math.floor(Math.random() * (max - min + 1) + min); // Generates random int value from min to max.
+      int min = 32; // Minimum value
+
+      final int MAX = 16384; // Maximum value
+
+      int random_int = (int)Math.floor(Math.random() * (MAX - min + 1) + min); // Generates random int value from min to max.
 
     // --------------------------------------------------------------------------------------
     //  Finally, this code takes everything and creates a unique string based on user inputs.
     // --------------------------------------------------------------------------------------
 
-    System.out.println("Your new string is:" + celsius + reverse + random_int); // Gives the user their new string.
+    System.out.println("Your new string is: " + celsius + reverse + random_int); // Gives the user their new string.
 
-    string.close();
 input.close();
 
  }
 }
+
+
+
+
+
+
+
+
+
+
+
 
 
