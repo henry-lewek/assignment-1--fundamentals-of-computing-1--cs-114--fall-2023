c1952d65e22baa90848678675845ce4e9e066a09
henry-lewek <lewek@hartford.edu>
2023-09-20 21:51:16 -0400

    finalized formatting

--- Assignment1.java
+++ Assignment1.java
@@ -16,18 +16,27 @@ public class Assignment1 {
     int max;
     int random_int;
 
-    System.out.println("                       "); // Placing space before printing to look cleaner in terminal.
+    // Placing space before printing to look cleaner in terminal.
+    System.out.println("                       ");
 
-    System.out.println("HHHHH    HHHHH   LLLLLL");
-    System.out.println("HHHHH    HHHHH   LLLLLL");
-    System.out.println("HHHHH    HHHHH   LLLLLL");
-    System.out.println("HHHHHHHHHHHHHH   LLLLLL");
-    System.out.println("HHHHHHHHHHHHHH   LLLLLL");
-    System.out.println("HHHHH    HHHHH   LLLLLL");
-    System.out.println("HHHHH    HHHHH   LLLLLLLLLLLL");
-    System.out.println("HHHHH    HHHHH   LLLLLLLLLLLL");
+      System.out.println("HHHHH    HHHHH   LLLLLL");
 
-    System.out.println("                       "); // Placing space after printing to look cleaner in terminal.
+      System.out.println("HHHHH    HHHHH   LLLLLL");
+
+      System.out.println("HHHHH    HHHHH   LLLLLL");
+
+      System.out.println("HHHHHHHHHHHHHH   LLLLLL");
+
+      System.out.println("HHHHHHHHHHHHHH   LLLLLL");
+
+      System.out.println("HHHHH    HHHHH   LLLLLL");
+
+      System.out.println("HHHHH    HHHHH   LLLLLLLLLLLL");
+
+      System.out.println("HHHHH    HHHHH   LLLLLLLLLLLL");
+
+    // Placing space after printing to look cleaner in terminal.
+    System.out.println("                       ");
 
     // -----------------------------------------
     //  Code that coverts Fahrenheit to Celcius.
@@ -35,11 +44,13 @@ public class Assignment1 {
 
     Scanner input = new Scanner(System.in);
 
-      System.out.println("Please input a degree in Fahrenheit to convert to Celsius: "); // This line is asking for the user to input a number.
+      // This line is asking for the user to input a number.
+      System.out.println("Please input a degree in Fahrenheit to convert to Celsius: ");
 
       fahrenheit = input.nextDouble();
 
-      celsius =(( 5 *(fahrenheit - 32.0)) / 9.0); // Equation for converting F to C.
+      // Equation for converting F to C.
+      celsius =(( 5 *(fahrenheit - 32.0)) / 9.0);
 
     System.out.println("                       ");
 
@@ -47,13 +58,16 @@ public class Assignment1 {
     //  Code that allows the user to input a 5-character string, then reverses it.
     // ---------------------------------------------------------------------------
 
-    System.out.println("Next, please input a 5-character string:"); //Prompts user for 5 characters.
+    //Prompts user for 5 characters.
+    System.out.println("Next, please input a 5-character string:");
 
       character = input.next();
 
-      result = character.substring(1,4); // Removes first and last character in the string.
+      // Removes first and last character in the string.
+      result = character.substring(1,4);
 
-      reverse = new StringBuilder(result).reverse().toString(); // Reverses string.
+      // Reverses string.
+      reverse = new StringBuilder(result).reverse().toString();
 
       System.out.println("                       ");
 
@@ -65,17 +79,21 @@ public class Assignment1 {
 
     System.out.println("Generating random number...");
 
-      min = 32; // Minimum value
+      // Minimum value
+      min = 32;
 
-      max = 16384; // Maximum value
+      // Maximum value
+      max = 16384;
 
-      random_int = (int)Math.floor(Math.random() * (max - min + 1) + min); // Generates random int value from min to max.
+      // Generates random int value from min to max.
+      random_int = (int)Math.floor(Math.random() * (max - min + 1) + min);
 
     // --------------------------------------------------------------------------------------
     //  Finally, this code takes everything and creates a unique string based on user inputs.
     // --------------------------------------------------------------------------------------
 
-    System.out.println("Your new string is: " + celsius + reverse + random_int); // Gives the user their new string.
+    // Gives the user their new string.
+    System.out.println("Your new string is: " + celsius + reverse + random_int);
 
 input.close();
 
